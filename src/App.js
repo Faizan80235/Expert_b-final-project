eb6acb3d3760ea2e59af4e0903d4902b2c52e9e5
Faizan80235 <fa3031980@gmail.com>
2025-01-08 02:03:33 +0500

    Update my project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import Rout from './Dashboard/Routes/Rout'
-// import Sidebars from "./Dashboard/Sidebars"
+
 function App() {
   return (
     <div>
       <Rout></Rout>
-    {/* <Sidebars></Sidebars> */}
+    
     </div>
   )
 }
